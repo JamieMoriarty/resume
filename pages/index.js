0c6aae654566ceb935b5a7e9c4a478b395c95d45
Martin <martin.doesnt.exist@gmail.com>
2021-02-01 11:45:09 +0100

    Less silly text on the front-page

--- pages/index.js
+++ pages/index.js
@@ -1,37 +1,31 @@
-import Head from "next/head";
-import styles from "styles/Home.module.scss";
+import Head from 'next/head';
+import styles from 'styles/Home.module.scss';
 
-import FrontPageCard from "@/components/frontpage/Card";
-import Resume from "@/components/resume";
+import FrontPageCard from '@/components/frontpage/Card';
+import Resume from '@/components/resume';
 
 export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Martin S. Løyche</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    return (
+        <div className={styles.container}>
+            <Head>
+                <title>Martin S. Løyche</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>Welcome!</h1>
+            <main className={styles.main}>
+                <h1 className={styles.title}>Welcome!</h1>
 
-        <div className={styles.description}>
-          <p>
-            This will initially be all playground for me and hopefully
-            transition to a display case for my talents along the way :)
-          </p>
-          <p>For now, it includes a resumé, and not much else.</p>
+                <div className={styles.description}>
+                    <p>Work is in progress on this page.</p>
+                    <p>You can find a resumé-like summary of my professional experience below. More things to come.</p>
+                </div>
+
+                <div className={styles.grid}>
+                    <FrontPageCard title="Resumé" description="A brief description of everything relevant to future employers :)">
+                        <Resume />
+                    </FrontPageCard>
+                </div>
+            </main>
         </div>
-
-        <div className={styles.grid}>
-          <FrontPageCard
-            title="Resumé"
-            description="A brief description of everything relevant to future employers :)"
-          >
-            <Resume />
-          </FrontPageCard>
-        </div>
-      </main>
-    </div>
-  );
+    );
 }
