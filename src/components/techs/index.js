0a24c749fb901274bf2b7fa09ba84dbf1e514ce5
Martin Søndergaard Løyche <martin.doesnt.exist@gmail.com>
2021-02-03 14:46:16 +0100

    Restructured scss a bit and styled tech categories a bit nicer

--- src/components/techs/index.js
+++ src/components/techs/index.js
@@ -3,22 +3,25 @@ import React from 'react';
 // Data:
 import techExperience from 'data/techExperience.json'
 
+// styles
+import styles from './Techs.module.scss'
+
+const capitalize = (string) => string.charAt(0).toUpperCase() + string.slice(1).toLowerCase()
+
 const Techs = () => {
     return (
         <article>
             {Object.keys(techExperience).map(category => (
-                <React.Fragment key={`tech_category_${category}`}>
-                    <h1>{category}</h1>
-                    <p>
-                        <ul>
-                            {techExperience[category].map(item => (
-                                <li key={`tech_category_${category}_${item}`}>
-                                    {item}
-                                </li>
-                            ))}
-                        </ul>
-                    </p>
-                </React.Fragment>
+                <section key={`tech_category_${category}`}>
+                    <h1 className={styles['techs-module__category-title']}>{capitalize(category)}</h1>
+                    <ul className={styles['techs-module__category-list']}>
+                        {techExperience[category].map(item => (
+                            <li key={`tech_category_${category}_${item}`} className={styles[category === 'languages' ? 'techs-module__category-item-3' : 'techs-module__category-item-2']}>
+                                {item}
+                            </li>
+                        ))}
+                    </ul>
+                </section>
             ))}
         </article>
     )
