d6c82e8030c2c39742118e6c83e751a7317bb4a3
Martin <martin.doesnt.exist@gmail.com>
2021-02-12 09:56:55 +0100

    Starting to get a structure for tech experience in place. Styling incomplete

--- src/components/techs/index.js
+++ src/components/techs/index.js
@@ -1,30 +1,52 @@
 import React from 'react';
 
 // Data:
-import techExperience from 'data/techExperience.json'
+import techExperience from 'data/techExperience.json';
 
 // styles
-import styles from './Techs.module.scss'
+import styles from './Techs.module.scss';
 
-const capitalize = (string) => string.charAt(0).toUpperCase() + string.slice(1).toLowerCase()
+const capitalize = string => string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
 
 const Techs = () => {
     return (
         <article>
-            {Object.keys(techExperience).map(category => (
-                <section key={`tech_category_${category}`}>
-                    <h1 className={styles['techs-module__category-title']}>{capitalize(category)}</h1>
-                    <ul className={styles['techs-module__category-list']}>
-                        {techExperience[category].map(item => (
-                            <li key={`tech_category_${category}_${item}`} className={styles[category === 'languages' ? 'techs-module__category-item-3' : 'techs-module__category-item-2']}>
-                                {item}
-                            </li>
-                        ))}
-                    </ul>
-                </section>
-            ))}
+            <TechCategory title={techExperience.languages.title}>
+                <ul className={styles['techs-module__category-list']}>
+                    {techExperience.languages.items.map(item => (
+                        <li key={`tech_category_language_${item}`} className={styles['techs-module__category-item-3']}>
+                            {item}
+                        </li>
+                    ))}
+                </ul>
+            </TechCategory>
+            <TechCategory title={techExperience.frameworks.title}>
+                {Object.keys(techExperience.frameworks.items).map(language => (
+                    <React.Fragment key={`tech_category_frameworks_${language}`}>
+                        <h2>{language}</h2>
+                        <ul className={styles['techs-module__category-list']}>
+                            {techExperience.frameworks.items[language].map(item => (
+                                <li key={`tech_category_frameworks_${language}_${item}`} className={styles['techs-module__category-item-3']}>
+                                    {item}
+                                </li>
+                            ))}
+                        </ul>
+                    </React.Fragment>
+                ))}
+            </TechCategory>
         </article>
-    )
-}
+    );
+};
 
-export default Techs
+const TechCategory = ({ title, children }) => {
+    return (
+        <section>
+            <header>
+                <h1 className={styles['techs-module__category-title']}>{capitalize(title)}</h1>
+            </header>
+            {children}
+        </section>
+    );
+};
+
+export default Techs;
