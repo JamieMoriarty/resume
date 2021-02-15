4296207b22dee5036a8622dffe5e6c564e0ebf1b
Martin <martin.doesnt.exist@gmail.com>
2021-02-15 16:18:17 +0100

    Minor styling touch-ups

--- src/components/personal/index.js
+++ src/components/personal/index.js
@@ -3,13 +3,16 @@ import React from 'react';
 // data:
 import personalStuff from 'data/personalStuff.json';
 
+// scss:
+import styles from './Personal.module.scss';
+
 const PersonalStuff = () => {
     return (
-        <React.Fragment>
+        <div className={styles['personal-module']}>
             {personalStuff.content.map((paragraph, index) => (
                 <p key={`personal_stuff_${index}`}>{paragraph}</p>
             ))}
-        </React.Fragment>
+        </div>
     );
 };
 
