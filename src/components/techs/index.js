e4f64a26bf6ca950b428652cbad12dc22d1a5d09
Martin <martin.doesnt.exist@gmail.com>
2021-02-15 17:20:50 +0100

    Use bootstrap grid, rather than homebrewed list solution, to improve look on both desktop and mobile

--- src/components/techs/index.js
+++ src/components/techs/index.js
@@ -11,31 +11,32 @@ const Techs = () => {
     return (
         <section>
             <TechCategory title={techExperience.languages.title}>
-                <ul className={styles['techs-module__category-list']}>
-                    {techExperience.languages.items.map(item => (
-                        <li
-                            key={`tech_category_language_${item}`}
-                            className={`${styles['techs-module__category-item']} ${styles['techs-module__category-item--3']}`}
-                        >
-                            {item}
-                        </li>
-                    ))}
-                </ul>
+                <div className="container">
+                    <div className={`row ${styles['techs-module__category-list']}`}>
+                        {techExperience.languages.items.map(item => (
+                            <div key={`tech_category_language_${item}`} className={`${styles['techs-module__category-item']} col-6 col-md-4`}>
+                                {item}
+                            </div>
+                        ))}
+                    </div>
+                </div>
             </TechCategory>
             <TechCategory title={techExperience.frameworks.title}>
                 {Object.keys(techExperience.frameworks.items).map(language => (
                     <React.Fragment key={`tech_category_frameworks_${language}`}>
                         <h2 className={styles['techs-module__category-subtitle']}>{language}</h2>
-                        <ul className={styles['techs-module__category-list']}>
-                            {techExperience.frameworks.items[language].map(item => (
-                                <li
-                                    key={`tech_category_frameworks_${language}_${item}`}
-                                    className={`${styles['techs-module__category-item']} ${styles['techs-module__category-item--3']}`}
-                                >
-                                    {item}
-                                </li>
-                            ))}
-                        </ul>
+                        <div className="container">
+                            <div className={`row ${styles['techs-module__category-list']}`}>
+                                {techExperience.frameworks.items[language].map(item => (
+                                    <div
+                                        key={`tech_category_frameworks_${language}_${item}`}
+                                        className={`${styles['techs-module__category-item']} col-6 col-md-4`}
+                                    >
+                                        {item}
+                                    </div>
+                                ))}
+                            </div>
+                        </div>
                     </React.Fragment>
                 ))}
             </TechCategory>
